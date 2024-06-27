0437b925a9646c740d8448e040012bf1fb02450e
YuyaShimomura <202404ap01_shimomura@x-ide.sakura.ne.jp>
2024-06-27 14:54:39 +0900

    #22702
    案内日(終了日付)が入力されていないにもかかわらずエラーメッセージが表示されない
    15、16行目を追加

--- SRC/js/guide.js
+++ SRC/js/guide.js
@@ -12,6 +12,8 @@ function fnGuideEditCheck() {
 		if (tmp.value == '') { alert(msg + "案内日(開始日付)を入力してください"); return; }
 		if (!fnYMDCheck(msg + "開始日付には正しい日付", tmp)) { return; }
 		if (!fnYMDCheck(msg + "開始日付には正しい日付", getEditObject(i, "guideStartDT"))) { return; }
+		tmp = getEditObject(i, "guideEndDT");
+		if (tmp.value == '') { alert(msg + "案内日(終了日付)を入力してください"); return; }
 		if (!fnYMDCheck(msg + "終了日付には正しい日付", getEditObject(i, "guideEndDT"))) { return; }
 		if (isLength(100, msg + "担当", getEditObject(i, "charge"))) { return; }
 		if (isLength(100, msg + "営業店", getEditObject(i, "name"))) { return; }
